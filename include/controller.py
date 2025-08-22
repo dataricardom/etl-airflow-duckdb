d68cc59ec5514399666968cb1f966a943de26df8
Ricardo Marques <dataricardom@gmail.com>
2025-08-22 01:46:25 -0300

    Criando Dag de processamento de API e armazenamento em Banco Postgres

---
+++ include/controller.py
@@ -0,0 +1,33 @@
+import requests
+from .db import SessionLocal, engine, Base
+from .models import Pokemon
+from .schema import PokemonSchema
+from random import randint
+
+Base.metadata.create_all(bind=engine)
+
+def gerar_numero_aleatorio():
+    return randint(1,350)
+
+def fetch_pokemon_data(pokemon_id: int):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
+    if response.status_code == 200:
+        data = response.json()
+        types = ', '.join(type_['type']['name'] for type_ in data['types'])
+        # Converte para dict antes de retornar
+        return PokemonSchema(name=data['name'], type=types).model_dump()
+    else:
+
+        return None
+
+
+def add_pokemon_to_db(pokemon_data_dict: dict) -> Pokemon:
+    
+    # Recria o objeto Pydantic a partir do dicionário
+    pokemon_schema = PokemonSchema(**pokemon_data_dict)
+    with SessionLocal() as db:
+        db_pokemon = Pokemon(name=pokemon_schema.name, type=pokemon_schema.type)
+        db.add(db_pokemon)
+        db.commit()
+        db.refresh(db_pokemon)
+    return db_pokemon
